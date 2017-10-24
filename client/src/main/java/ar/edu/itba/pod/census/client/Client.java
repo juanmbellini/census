a88d1fdb5e77d79573641ab29bb0ecbe087e46bf
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-10-24 16:13:43 -0300

    Add project structure

---
+++ client/src/main/java/ar/edu/itba/pod/census/client/Client.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.pod.census.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Client.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("census Client Starting ...");
+    }
+}
---
+++ server/src/main/java/ar/edu/itba/pod/census/server/Server.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.pod.census.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("census Server Starting ...");
+    }
+}
