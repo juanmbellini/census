90430a77b7398ceb2ee7d509369c83f0c3be53fd
Martin Goffan <martin.goffan@me.com>
2017-11-04 18:09:31 -0300

    Added naive file writer

---
+++ client/src/main/java/ar/edu/itba/pod/census/client/file/NaiveWriter.java
@@ -0,0 +1,30 @@
+package ar.edu.itba.pod.census.client.file;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+import org.slf4j.Logger;
+
+public class NaiveWriter {
+	private String _fileName;
+  private Logger _logger;
+
+  public NaiveWriter(String fileName, Logger logger) {
+    _fileName = fileName;
+    _logger = logger;
+  }
+
+  public void write(List<String> lines) {
+    Path path = Paths.get(_fileName);
+    try {
+      Files.write(path, lines, Charset.forName("UTF-8"), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    } catch (IOException e) {
+      _logger.error("couldn't create output file " + _fileName);
+    }
+  }
+}
