c6b7fdd4abac0f32203a9a437959397057ca3fe3
jpascale <jpascale@itba.edu.ar>
2017-11-05 02:09:13 -0300

    Add initial hazelcast conn to cluster

--- client/src/main/java/ar/edu/itba/pod/census/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/census/client/Client.java
@@ -1,12 +1,30 @@
 package ar.edu.itba.pod.census.client;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class Client {
     private final static Logger LOGGER = LoggerFactory.getLogger(Client.class);
 
     public static void main(String[] args) {
         LOGGER.info("census Client Starting ...");
+        ClientConfig config = new ClientConfig();
+        GroupConfig groupConfig = config.getGroupConfig();
+        groupConfig.setName("dev");
+        groupConfig.setPassword("dev-pass");
+        HazelcastInstance hazelcastClient = HazelcastClient.newHazelcastClient(config);
+
+        IMap<Long, String> map = hazelcastClient.getMap("data");
+
+        for (Map.Entry<Long, String> entry : map.entrySet()) {
+            LOGGER.info(entry.getKey() + " - " + entry.getValue());
+        }
     }
 }
---
+++ clt.sh
@@ -0,0 +1,5 @@
+cd client/target
+tar -xvf census-client-1.0-SNAPSHOT-bin.tar.gz
+chmod 777 census-client-1.0-SNAPSHOT/run-client.sh
+cd census-client-1.0-SNAPSHOT
+./run-client.sh
