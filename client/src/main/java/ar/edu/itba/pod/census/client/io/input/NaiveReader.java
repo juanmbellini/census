c7b10ff621f1801aa38db5fd532f19c18b708ef6
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-11-04 22:00:26 -0300

    Apply code styling + javadocs

--- client/src/main/java/ar/edu/itba/pod/census/client/io/input/NaiveReader.java
+++ client/src/main/java/ar/edu/itba/pod/census/client/io/input/NaiveReader.java
@@ -1,28 +1,51 @@
 package ar.edu.itba.pod.census.client.io.input;
 
-import com.hazelcast.core.IMap;
-
 import ar.edu.itba.pod.census.api.models.Citizen;
+import com.hazelcast.core.IMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static java.lang.System.exit;
+import java.io.IOException;
 
+/**
+ * Naive reader.
+ */
 public class NaiveReader {
-	private String _inputFile;
 
-  public NaiveReader(String inputFile) {
-    _inputFile = inputFile;
-  }
+    private final static Logger LOGGER = LoggerFactory.getLogger(NaiveReader.class);
 
-  public void loadData(final IMap<String, Citizen> iMap) {
-    try {
-      CensusReader.readCitizens(_inputFile, iMap);
-    } catch (Exception e) {
-      System.out.println("File or directory doesn't exist");
-      exit(1);
+    /**
+     * The path to the input file.
+     */
+    private final String inputFile;
+
+    /**
+     * Constructor.
+     *
+     * @param inputFile The path to the input file.
+     */
+    public NaiveReader(String inputFile) {
+        this.inputFile = inputFile;
     }
-  }
 
-  public String getInputFile() {
-    return _inputFile;
-  }
+    /**
+     * Loads data into the given {@link IMap}.
+     *
+     * @param iMap The {@link IMap} to which data must be loaded into.
+     */
+    public void loadData(final IMap<Integer, Citizen> iMap) throws IOException {
+        try {
+            CensusReader.readCitizens(inputFile, iMap);
+            CensusReader.readCitizens(inputFile, iMap);
+        } finally {
+            LOGGER.error("File or directory doesn't exist");
+        }
+    }
+
+    /**
+     * @return The path to the input file.
+     */
+    public String getInputFile() {
+        return inputFile;
+    }
 }
