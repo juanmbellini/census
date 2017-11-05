f9b81181d4b9e705b3d9896a5cb9f37f4ee8dab0
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-11-04 21:31:27 -0300

    Add JEasy to InputParams
    - Use JEasy library to parse system properties

--- client/src/main/java/ar/edu/itba/pod/census/client/io/cli/InputParams.java
+++ client/src/main/java/ar/edu/itba/pod/census/client/io/cli/InputParams.java
@@ -1,58 +1,134 @@
 package ar.edu.itba.pod.census.client.io.cli;
 
+import org.jeasy.props.PropertiesInjectorBuilder;
+import org.jeasy.props.annotations.SystemProperty;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public class InputParams {
-	
-	private String name;
-  private String pass;
-  private String[] addresses;
-  private Integer queryID;
-  private String inputPath;
-  private String outputPath;
+
+    private String name;
+    private String pass;
 
 
-  public InputParams(String name, String pass, String[] addresses,  Integer queryID, String inputPath, String outputPath) {
-    this.name = name;
-    this.pass = pass;
-    this.addresses = addresses;
-    this.queryID = queryID;
-    this.inputPath = inputPath;
-    this.outputPath = outputPath;
-  }
+    // =======================================
+    // Cluster arguments
+    // =======================================
+    @SystemProperty(value = "addresses")
+    private String[] addresses;
 
-  public String getName() {
-    return name;
-  }
+    // =======================================
+    // Files arguments
+    // =======================================
+    @SystemProperty(value = "inPath", defaultValue = "census.csv")
+    private String dataFilePath = "census.csv";
 
-  public String getPass() {
-    return pass;
-  }
+    @SystemProperty(value = "outPath", defaultValue = "output.txt")
+    private String outputFilePath = "output.txt";
 
-  public String[] addresses() {
-    return addresses;
-  }
+    @SystemProperty(value = "timeOutPath", defaultValue = "time.txt")
+    private String timestampsFilePath = "time.txt";
 
-  public Integer getQueryID() {
-    return queryID;
-  }
+    // =======================================
+    // Execution arguments
+    // =======================================
+    @SystemProperty(value = "query")
+    private int queryId;
 
-  public String getInputPath() {
-    return inputPath;
-  }
+    // =======================================
+    // Query param arguments
+    // =======================================
 
-  public String getOutputPath() {
-    return outputPath;
-  }
+    @SystemProperty(value = "n")
+    private Integer n;
 
-  @Override
-  public String toString() {
-    String string = "";
-    string += "name: " + name + "\n";
-    string += "pass: " + pass + "\n";
-    string += "address:" + addresses + "\n";
-    string += "query: " + queryID + "\n";
-    string += "input path: " + inputPath + "\n";
-    string += "output path: " + outputPath + "\n";
+    @SystemProperty(value = "prov")
+    private String prov;
 
-    return string;
-  }
+
+    /**
+     * Default constructor that uses JEasy to initialize variables.
+     */
+    public InputParams() {
+        PropertiesInjectorBuilder.aNewPropertiesInjector().injectProperties(this);
+
+        // TODO: validate arguments
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+
+    /**
+     * @return The list of member node ip addresses.
+     */
+    public String[] getAddresses() {
+        return addresses;
+    }
+
+    /**
+     * @return The path of the data file to be processed.
+     */
+    public String getDataFilePath() {
+        return dataFilePath;
+    }
+
+    /**
+     * @return The path to the output file.
+     */
+    public String getOutputFilePath() {
+        return outputFilePath;
+    }
+
+    /**
+     * @return The path to the timestamps file.
+     */
+    public String getTimestampsFilePath() {
+        return timestampsFilePath;
+    }
+
+    /**
+     * @return The id of the query being executed.
+     */
+    public int getQueryId() {
+        return queryId;
+    }
+
+    /**
+     * @return The 'n' query param.
+     */
+    public Integer getN() {
+        return n;
+    }
+
+    /**
+     * @return The 'prov' query param.
+     */
+    public String getProv() {
+        return prov;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("name: ").append(name).append("\n");
+        stringBuilder.append("pass: ").append(pass).append("\n");
+        stringBuilder.append("addresses: ").append(Arrays.toString(addresses)).append("\n");
+        stringBuilder.append("data file path: ").append(dataFilePath).append("\n");
+        stringBuilder.append("output path: ").append(outputFilePath).append("\n");
+        stringBuilder.append("timestamps output path: ").append(timestampsFilePath).append("\n");
+        stringBuilder.append("queryId: ").append(queryId).append("\n");
+        Optional.ofNullable(this.n)
+                .ifPresent(value -> stringBuilder.append("Query param n: ").append(value).append("\n"));
+        Optional.ofNullable(this.prov)
+                .ifPresent(value -> stringBuilder.append("Query param prov: ").append(value).append("\n"));
+
+        return stringBuilder.toString();
+    }
 }
