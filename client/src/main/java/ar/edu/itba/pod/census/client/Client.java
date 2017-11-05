5d82de44c0b7f5c26fe80b2ad13421ba3ba11d4b
jpascale <jpascale@itba.edu.ar>
2017-11-05 02:39:09 -0300

    Make IP config explicit

--- client/src/main/java/ar/edu/itba/pod/census/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/census/client/Client.java
@@ -2,7 +2,8 @@ package ar.edu.itba.pod.census.client;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.config.GroupConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.config.*;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import org.slf4j.Logger;
@@ -16,10 +17,21 @@ public class Client {
     public static void main(String[] args) {
         LOGGER.info("census Client Starting ...");
         ClientConfig config = new ClientConfig();
+
         GroupConfig groupConfig = config.getGroupConfig();
         groupConfig.setName("dev");
         groupConfig.setPassword("dev-pass");
+
+        // Asi se setean todas las IP que vienen por parametro, se le puede mandar una lista/array/etc
+        ClientNetworkConfig networkConfig = new ClientNetworkConfig();
+        networkConfig.addAddress("127.0.0.1:5701");
+        config.setNetworkConfig(networkConfig);
+
+
         HazelcastInstance hazelcastClient = HazelcastClient.newHazelcastClient(config);
+
+        // ManagementCenterConfig manCenterCfg = new ManagementCenterConfig();
+        // manCenterCfg.setEnabled(true).setUrl("http://localhost:8080/mancenter");
 
         IMap<Long, String> map = hazelcastClient.getMap("data");
 
