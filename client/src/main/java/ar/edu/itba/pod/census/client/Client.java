7127f6115f16192687495ae74d91aace501866ee
jpascale <jpascale@itba.edu.ar>
2017-11-05 02:48:54 -0300

    Add hazelcast basic scaffolding

--- client/src/main/java/ar/edu/itba/pod/census/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/census/client/Client.java
@@ -1,12 +1,42 @@
 package ar.edu.itba.pod.census.client;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class Client {
     private final static Logger LOGGER = LoggerFactory.getLogger(Client.class);
 
     public static void main(String[] args) {
         LOGGER.info("census Client Starting ...");
+        ClientConfig config = new ClientConfig();
+
+        GroupConfig groupConfig = config.getGroupConfig();
+        groupConfig.setName("dev");
+        groupConfig.setPassword("dev-pass");
+
+        // Asi se setean todas las IP que vienen por parametro, se le puede mandar una lista/array/etc
+        ClientNetworkConfig networkConfig = new ClientNetworkConfig();
+        networkConfig.addAddress("127.0.0.1:5701");
+        config.setNetworkConfig(networkConfig);
+
+
+        HazelcastInstance hazelcastClient = HazelcastClient.newHazelcastClient(config);
+
+        // ManagementCenterConfig manCenterCfg = new ManagementCenterConfig();
+        // manCenterCfg.setEnabled(true).setUrl("http://localhost:8080/mancenter");
+
+        IMap<Long, String> map = hazelcastClient.getMap("data");
+
+        for (Map.Entry<Long, String> entry : map.entrySet()) {
+            LOGGER.info(entry.getKey() + " - " + entry.getValue());
+        }
     }
 }
--- server/src/main/java/ar/edu/itba/pod/census/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/census/server/Server.java
@@ -1,12 +1,23 @@
 package ar.edu.itba.pod.census.server;
 
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IdGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class Server {
     private final static Logger LOGGER = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) {
         LOGGER.info("census Server Starting ...");
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        Map<Long, String> map = hazelcastInstance.getMap("data");
+        IdGenerator idGenerator = hazelcastInstance.getIdGenerator("newid");
+        for (int i = 0; i < 100000; i++) {
+            map.put(idGenerator.newId(), "message" + 1);
+        }
     }
 }
