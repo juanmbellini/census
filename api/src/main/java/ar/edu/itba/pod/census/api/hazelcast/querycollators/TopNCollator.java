584f84dcf1c00b7a084c285b8c103241d9442f75
Martin Goffan <mgoffan@itba.edu.ar>
2017-11-06 18:44:49 -0300

    Fixed query2

--- api/src/main/java/ar/edu/itba/pod/census/api/hazelcast/querycollators/TopNCollator.java
+++ api/src/main/java/ar/edu/itba/pod/census/api/hazelcast/querycollators/TopNCollator.java
@@ -7,9 +7,17 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.hazelcast.mapreduce.Collator;
 
 public class TopNCollator<T> implements Collator<Entry<T, Long>, Map<T, Long>> {
+	
+	/**
+   * The {@link Logger}.
+   */
+  private final static Logger LOGGER = LoggerFactory.getLogger(TopNCollator.class);
 	
 	private Integer n;
 	
@@ -20,18 +28,27 @@ public class TopNCollator<T> implements Collator<Entry<T, Long>, Map<T, Long>> {
 	@Override
 	public Map<T, Long> collate(Iterable<Entry<T, Long>> arg0) {
 		
+		LOGGER.info("Running collator");
+		
 		final Map<T, Long> m = new HashMap<>();
 		
 		for (Entry<T, Long> e : arg0) {
+			LOGGER.trace(e.getKey() + ": " + e.getValue());
 			m.put(e.getKey(), e.getValue());
 		}
 		
-		return m.entrySet().stream()
+		LOGGER.debug("Input: {}", m);
+		
+		final Map<T, Long> result = m.entrySet().stream()
         .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
         .limit(this.n)
         .collect(
         		Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new)
         );
+		
+		LOGGER.debug("Result: {}", result);
+		
+		return result;
 	}
 
 }
