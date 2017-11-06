51562d4e8db228816152b09db22c65d7f8b5754f
Martin Goffan <martin.goffan@me.com>
2017-11-05 21:51:28 -0300

    Basic (copy) creation of Query2. Added TopCollator

---
+++ api/src/main/java/ar/edu/itba/pod/census/api/hazelcast/querycombiners/Query2Combiner.java
@@ -0,0 +1,34 @@
+package ar.edu.itba.pod.census.api.hazelcast.querycombiners;
+
+import com.hazelcast.mapreduce.Combiner;
+import com.hazelcast.mapreduce.CombinerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Query2Combiner implements CombinerFactory<String, Long, Long> {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Query2Combiner.class);
+    @Override
+    public Combiner<Long, Long> newCombiner(String key ) {
+        return new RegionCombiner();
+    }
+
+    private class RegionCombiner extends Combiner <Long, Long> {
+        private volatile long sum = 0;
+
+        @Override
+        public void combine(Long val) {
+            sum += val;
+        }
+
+        @Override
+        public Long finalizeChunk() {
+            LOGGER.info("Combiner -> Returning {}", sum);
+            return sum;
+        }
+
+        @Override
+        public void reset() {
+            sum = 0;
+        }
+    }
+}
---
+++ api/src/main/java/ar/edu/itba/pod/census/api/hazelcast/queryreducers/Query2Reducer.java
@@ -0,0 +1,36 @@
+package ar.edu.itba.pod.census.api.hazelcast.queryreducers;
+
+import com.hazelcast.mapreduce.Reducer;
+import com.hazelcast.mapreduce.ReducerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Query2Reducer implements ReducerFactory<String, Long, Long> {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Query2Reducer.class);
+
+    @Override
+    public Reducer<Long, Long> newReducer(String s) {
+        return new RegionCounter();
+    }
+
+    private class RegionCounter extends Reducer<Long, Long> {
+        private volatile long sum;
+
+        @Override
+        public void beginReduce() {
+            sum = 0;
+        }
+
+        @Override
+        public void reduce(Long value) {
+            sum += value;
+        }
+
+        @Override
+        public Long finalizeReduce() {
+            LOGGER.info("Reducer returning {} for Query1", sum);
+            return sum;
+        }
+
+    }
+}
