c6d427ac6d32c77b01cfde872b9015005729a929
Martin Goffan <mgoffan@itba.edu.ar>
2017-11-06 18:09:49 -0300

    clt.sh passthrough param fix

--- clt.sh
+++ clt.sh
@@ -2,4 +2,4 @@ cd client/target
 tar -xvf census-client-1.0-SNAPSHOT-bin.tar.gz
 chmod 777 census-client-1.0-SNAPSHOT/run-client.sh
 cd census-client-1.0-SNAPSHOT
-./run-client.sh
+./run-client.sh $*
