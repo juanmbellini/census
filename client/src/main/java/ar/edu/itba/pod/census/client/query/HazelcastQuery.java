4e5f6d615ae3c05956dcd9253ec1e562f27dcb12
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-11-06 00:47:52 -0300

    Create Abstract HazelcastQuery
    - Base class fro which all queries should extend

---
+++ client/src/main/java/ar/edu/itba/pod/census/client/query/HazelcastQuery.java
@@ -0,0 +1,118 @@
+package ar.edu.itba.pod.census.client.query;
+
+import ar.edu.itba.pod.census.api.models.Citizen;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.util.UuidUtil;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+/**
+ * An Abstract {@link Query} that uses Hazelcast to perform them.
+ *
+ * @param <K> The type of the key of the resulting map of the query.
+ * @param <V> The type of the value of the resulting map of the query.
+ */
+public abstract class HazelcastQuery<K, V> implements Query<K, V> {
+
+    /**
+     * The {@link HazelcastInstance} to be used to perform queries.
+     */
+    private final HazelcastInstance hazelcastInstance;
+
+
+    /**
+     * @param hazelcastInstance The {@link HazelcastInstance} from which the {@link IMap} is constructed.
+     */
+    protected HazelcastQuery(HazelcastInstance hazelcastInstance) {
+        this.hazelcastInstance = hazelcastInstance;
+    }
+
+
+    @Override
+    public Map<K, V> perform(List<Citizen> citizens) {
+        return perform(toHazelcastIMap(citizens));
+    }
+
+    /**
+     * Method that actually performs the query.
+     *
+     * @param citizensIMap The {@link IMap} holding the {@link Citizen} data to be processed.
+     * @return A {@link Map} holding the query results.
+     */
+    protected abstract Map<K, V> perform(IMap<String, Citizen> citizensIMap);
+
+
+    /**
+     * Transforms the given {@link List} of {@link Citizen} into an {@link IMap} with key of type {@code E},
+     * and value of type {@link Citizen}.
+     *
+     * @param citizens The {@link List} of {@link Citizen} to be transformed.
+     * @return The generated {@link IMap}.
+     */
+    private IMap<String, Citizen> toHazelcastIMap(List<Citizen> citizens) {
+        final IMap<String, Citizen> hazelcastMap = hazelcastInstance.getMap(hazelcastInstance.getName());
+        return citizens.stream().collect(new ToIMapCollector<>(UuidUtil::newSecureUuidString, hazelcastMap));
+
+    }
+
+    /**
+     * {@link Collector} that will return all input data into a Hazelcast {@link IMap}.
+     *
+     * @param <T> The type of the key to be used in the resultant {@link IMap}.
+     */
+    private final class ToIMapCollector<T> implements Collector<Citizen, Map<T, Citizen>, IMap<T, Citizen>> {
+
+        /**
+         * {@link Supplier} of keys.
+         */
+        private final Supplier<T> keyGenerator;
+
+        /**
+         * Resultant {@link IMap}, which will hold the input data.
+         */
+        private final IMap<T, Citizen> iMap;
+
+        private ToIMapCollector(Supplier<T> keyGenerator, IMap<T, Citizen> iMap) {
+            this.keyGenerator = keyGenerator;
+            this.iMap = iMap;
+        }
+
+        @Override
+        public Supplier<Map<T, Citizen>> supplier() {
+            return HashMap::new;
+        }
+
+        @Override
+        public BiConsumer<Map<T, Citizen>, Citizen> accumulator() {
+            return (map, citizen) -> map.put(keyGenerator.get(), citizen);
+        }
+
+        @Override
+        public BinaryOperator<Map<T, Citizen>> combiner() {
+            return (map1, map2) -> {
+                map1.putAll(map2);
+                return map1;
+            };
+        }
+
+        @Override
+        public Function<Map<T, Citizen>, IMap<T, Citizen>> finisher() {
+            return map -> {
+                iMap.putAll(map);
+                return iMap;
+            };
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Collections.singleton(Characteristics.CONCURRENT);
+        }
+    }
+
+}
