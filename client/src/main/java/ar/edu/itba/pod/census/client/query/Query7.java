8b1d65c726b72e6f72266914fc3a8c0db268cef5
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-11-07 10:12:09 -0300

    Remove unused CombinerFactory for Query 7

--- client/src/main/java/ar/edu/itba/pod/census/client/query/Query7.java
+++ client/src/main/java/ar/edu/itba/pod/census/client/query/Query7.java
@@ -4,7 +4,6 @@ import ar.edu.itba.pod.census.api.hazelcast.querycollators.TopWithMinNCollator;
 import ar.edu.itba.pod.census.api.hazelcast.querymappers.Query7Mapper;
 import ar.edu.itba.pod.census.api.hazelcast.queryreducers.Query7ReducerFactory;
 import ar.edu.itba.pod.census.api.models.Citizen;
-import ar.edu.itba.pod.census.api.util.StringPair;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.mapreduce.Job;
 
@@ -34,7 +33,6 @@ public class Query7 extends HazelcastQuery<String, Long> {
             throw new IllegalArgumentException("The n query param must be specified for query 7");
         }
         return job.mapper(new Query7Mapper<>())
-//				.combiner(new Query7CombinerFactory())
                 .reducer(new Query7ReducerFactory())
                 .submit(new TopWithMinNCollator<>(params.getN()))
                 .get().entrySet().stream()
