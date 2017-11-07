d24ee66910358430cc02da2510e6c36ef1a9f4a4
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-11-07 04:45:19 -0300

    Remove Logging from Combiners and CombinerFactories

--- api/src/main/java/ar/edu/itba/pod/census/api/hazelcast/querycombiners/Query3CombinerFactory.java
+++ api/src/main/java/ar/edu/itba/pod/census/api/hazelcast/querycombiners/Query3CombinerFactory.java
@@ -3,48 +3,38 @@ package ar.edu.itba.pod.census.api.hazelcast.querycombiners;
 import ar.edu.itba.pod.census.api.models.Region;
 import ar.edu.itba.pod.census.api.util.BooleanPair;
 import ar.edu.itba.pod.census.api.util.IntegerPair;
-import ar.edu.itba.pod.census.api.util.Pair;
 import com.hazelcast.mapreduce.Combiner;
 import com.hazelcast.mapreduce.CombinerFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Map;
 
 /**
  * {@link CombinerFactory} for the query 3
  * (i.e returns a {@link Combiner} that count elements of the same department name).
  */
 public class Query3CombinerFactory implements CombinerFactory<Region, BooleanPair, IntegerPair> {
-    
-    private final static Logger LOGGER = LoggerFactory.getLogger(Query3CombinerFactory.class);
-    
+
     /**
      * Used for serialization of this {@link CombinerFactory}.
      */
     private static final long serialVersionUID = 1L;
-    
+
     @Override
     public Combiner<BooleanPair, IntegerPair> newCombiner(Region region) {
-        LOGGER.trace("instantiating combiner");
         return new Combiner<BooleanPair, IntegerPair>() {
-            
+
             private int homeless;
             private int working;
-            
+
             @Override
             public void combine(BooleanPair workingHomelessPair) {
-                LOGGER.trace("combining {}", workingHomelessPair);
                 if (workingHomelessPair.getLeft()) {
                     working++;
                 } else if (workingHomelessPair.getRight()) {
                     homeless++;
                 }
             }
-    
+
             @Override
             public IntegerPair finalizeChunk() {
-                LOGGER.trace("finalize chunk {}/{}", working, homeless);
                 return new IntegerPair(working, homeless);
             }
         };
