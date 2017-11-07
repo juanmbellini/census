1922d36028666d62ed29ea35f8ce69d0613f96d6
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-11-07 00:28:23 -0300

    Add missing javadocs to util package classes

--- api/src/main/java/ar/edu/itba/pod/census/api/util/BooleanPair.java
+++ api/src/main/java/ar/edu/itba/pod/census/api/util/BooleanPair.java
@@ -1,8 +1,17 @@
 package ar.edu.itba.pod.census.api.util;
 
+/**
+ * A {@link Pair} of {@link Boolean}.
+ */
 public class BooleanPair extends Pair<Boolean, Boolean> {
-	
-	public BooleanPair(Boolean l, Boolean r) {
-		super(l, r);
-	}
+
+    /**
+     * Constructor.
+     *
+     * @param l The left {@link Boolean} of the {@link Pair}.
+     * @param r The right {@link Boolean} of the {@link Pair}.
+     */
+    public BooleanPair(Boolean l, Boolean r) {
+        super(l, r);
+    }
 }
--- api/src/main/java/ar/edu/itba/pod/census/api/util/Pair.java
+++ api/src/main/java/ar/edu/itba/pod/census/api/util/Pair.java
@@ -1,30 +1,60 @@
 package ar.edu.itba.pod.census.api.util;
 
-public class Pair<L,R> {
-	
-	private final L left;
-	private final R right;
-	
-	public Pair(L left, R right) {
-		this.left = left;
-		this.right = right;
-	}
-	
-	public L getLeft() { return left; }
-	public R getRight() { return right; }
-	
-	@Override
-	public int hashCode() { return left.hashCode() ^ right.hashCode(); }
-	
-	@Override
-	public boolean equals(Object o) {
-		if (!(o instanceof Pair)) return false;
-		Pair pairo = (Pair) o;
-		return this.left.equals(pairo.getLeft()) && this.right.equals(pairo.getRight());
-	}
-	
-	@Override
-	public String toString() {
-		return "(" + left.toString() + ", " + right.toString() + ")";
-	}
+/**
+ * Class representing a pair of objects.
+ *
+ * @param <L> The left side of the pair concrete type.
+ * @param <R> The right side of the pair concrete type.
+ */
+public class Pair<L, R> {
+
+    /**
+     * The left element of the pair.
+     */
+    private final L left;
+
+    /**
+     * The right element of the pair.
+     */
+    private final R right;
+
+    /**
+     * @param left  The left element of the pair.
+     * @param right The right element of the pair.
+     */
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    /**
+     * @return The left element of the pair.
+     */
+    public L getLeft() {
+        return left;
+    }
+
+    /**
+     * @return The right element of the pair.
+     */
+    public R getRight() {
+        return right;
+    }
+
+    @Override
+    public int hashCode() {
+        return left.hashCode() ^ right.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Pair)) return false;
+        final Pair other = (Pair) o;
+        return this.left.equals(other.getLeft()) && this.right.equals(other.getRight());
+    }
+
+    @Override
+    public String toString() {
+        return "(" + left.toString() + ", " + right.toString() + ")";
+    }
 }
