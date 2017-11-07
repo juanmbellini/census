9bba4c28932fcd750c59757e36886cb20fa39422
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-11-07 05:39:32 -0300

    Add missing javadocs for IntegerSet, LongSet and StringPair

--- api/src/main/java/ar/edu/itba/pod/census/api/util/LongSet.java
+++ api/src/main/java/ar/edu/itba/pod/census/api/util/LongSet.java
@@ -2,5 +2,8 @@ package ar.edu.itba.pod.census.api.util;
 
 import java.util.HashSet;
 
+/**
+ * A concrete extension of {@link HashSet} for {@link Long}.
+ */
 public class LongSet extends HashSet<Long> {
 }
--- api/src/main/java/ar/edu/itba/pod/census/api/util/StringPair.java
+++ api/src/main/java/ar/edu/itba/pod/census/api/util/StringPair.java
@@ -1,8 +1,17 @@
 package ar.edu.itba.pod.census.api.util;
 
+/**
+ * A {@link Pair} of {@link String}.
+ */
 public class StringPair extends Pair<String, String> {
-	
-	public StringPair(String l, String r) {
-		super(l, r);
-	}
+
+    /**
+     * Constructor.
+     *
+     * @param l The left {@link String} of the {@link Pair}.
+     * @param r The right {@link String} of the {@link Pair}.
+     */
+    public StringPair(String l, String r) {
+        super(l, r);
+    }
 }
